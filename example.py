2065febef6e71f45ae2743992950387aa229bb84
hugovk <hugovk@users.noreply.github.com>
2017-02-12 18:24:29 +0200

    Initial commit

---
+++ example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Example Python script
+"""
+from __future__ import print_function, unicode_literals
+
+
+def some_function(input):
+    """Print the input"""
+    print(input)
+
+
+if __name__ == "__main__":
+    some_function(123)
+
+# End of file
